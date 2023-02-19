4a962b52acae5caf96acbeda8794796c990acda4
vcff <34322438+vcff@users.noreply.github.com>
2023-02-20 00:58:58 +0100

    Update get_regkey_server.ps1

--- get_regkey_server.ps1
+++ get_regkey_server.ps1
@@ -6,46 +6,50 @@
 # Error handling to make sure file is present
 $ErrorActionPreference = "Stop"
 try {
-   if(Import-Csv -Path .\serverlist.csv) {
-      Write-Output "serverlist.csv found"
+   if(Import-Csv -Path .\servers.csv) {
+      Write-Output "servers.csv found"
    }
 }catch {
-   Write-Output "serverlist.csv not found"
+   Write-Output "servers.csv not found"
    $_.Exception
 }
-$servers = Import-Csv -Path .\serverlist.csv
+
+$servers = Import-Csv -Path .\servers.csv
 $user = "$computername\nt"
-# password is starred/hidden instead of plain text
 $password = Read-Host 'What is your password?' -AsSecureString
 $cred = [PSCredential]::new($user,$password)
-# this goes thru each server read from the CSV. By default, first row is treated as header.
 foreach ($line in $servers)
 { 
-    # When calling the computername, you refer the line, and to the particular column name. here we assume it is called "ServerName"
-    $result = Invoke-Command -Computername $line.ServerName -Credential $cred -ScriptBlock {
+    try {
+	$result = Invoke-Command -Computername $line.ServerName -Credential $cred -ScriptBlock  {
         $Hostname = (Hostname)
-	# to add another layer to make sure that script only process server versions based on Major,Minor,Type (BuildNumbers) skipped for simplicity
-	#Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion"
-	$CurrentMajorVersionNumber = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name CurrentMajorVersionNumber).CurrentMajorVersionNumber
-        $CurrentMinorVersionNumber = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name CurrentMinorVersionNumber).CurrentMinorVersionNumber
-        $InstallationType = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name InstallationType).InstallationType
-	$ProductName = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name ProductName).ProductName
+		#Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion"
+		$CurrentMajorVersionNumber = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name CurrentMajorVersionNumber -EA stop).CurrentMajorVersionNumber
+        $CurrentMinorVersionNumber = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name CurrentMinorVersionNumber -EA stop).CurrentMinorVersionNumber
+        $InstallationType = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name InstallationType -EA stop).InstallationType
+		$ProductName = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name ProductName -EA stop).ProductName
         #Windows Server 2016 / 2019 / 2022
-	If ($CurrentMajorVersionNumber -eq "10" -and $CurrentMinorVersionNumber -eq "0" -and $InstallationType -eq "Server")  {
+		If ($CurrentMajorVersionNumber -eq "10" -and $CurrentMinorVersionNumber -eq "0" -and $InstallationType -eq "Server")  {
             $TimeOutValue = (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Services\disk" -Name TimeOutValue).TimeOutValue
-		    Write-Host ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+		    Write-Host "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
 		    Write-Host "Given Windows Version is "$ProductName" located on "$Hostname  " " 
-		    Write-Host "Queried value "TimeOutValue" from registry Key for "HKLM:\SYSTEM\CurrentControlSet\Services\disk" is "$TimeOutValue" " 
-            }
+		    Write-Host "Queried value "TimeOutValue" from registry Key for "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" is "$TimeOutValue" "  
+            } 
         # Windows Server 2012 and R2 version
         ElseIf (($CurrentMajorVersionNumber -eq "6" -and $CurrentMinorVersionNumber -eq "2" -and $InstallationType -eq "Server") -Or ($CurrentMajorVersionNumber -eq "6" -and $CurrentMinorVersionNumber -eq "3" -and $InstallationType -eq "Server")) {
             $TimeOutValue = (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Services\disk" -Name TimeOutValue).TimeOutValue
-		    Write-Host ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+		    Write-Host "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
 		    Write-Host "Given Windows Version is "$ProductName" located on "$Hostname  " " 
-		    Write-Host "Queried value "TimeOutValue" from registry Key for "HKLM:\SYSTEM\CurrentControlSet\Services\disk" is "$TimeOutValue" " 
-            }
+		    Write-Host "Queried value "TimeOutValue" from registry Key for "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" is "$TimeOutValue" "  
+            } 
         Else {
         Write-Host "Given Windows Version is "$ProductName" located on "$Hostname and not Server edition" " 
-        } 
-    } -ErrorAction SilentlyContinue 
-    $result
+		# EA action will make sure thats if in defined server list one of server is online, script will continue with another one
+			} 
+		}
+    } #-ErrorAction SilentlyContinue 
+	catch [System.Management.Automation.Remoting.PSRemotingTransportException]  {
+    Write-Host " "$_.Exception" " -ForegroundColor Red
+    }
+    $result 
+}
